f7deabdb56127a62b000a65ea393c8c3f3ad01d4
pong3ds <chaiyapong@3dsinteractive.com>
2018-09-04 13:42:45 +0700

    Mutliple consume

--- main.go
+++ main.go
@@ -3,39 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/3dsinteractive/confluent-kafka-go/kafka"
 	"github.com/3dsinteractive/echo"
 )
 
+func consume(groupID string, worker string, topic string) {
+	workerName := groupID + ":" + worker
+	fmt.Printf("%s: Bootstrap Kafka with server %s\n", workerName, os.Getenv("HOST_IP_ADDRESS"))
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("HOST_IP_ADDRESS"),
+		"group.id":          groupID,
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.Subscribe(topic, nil)
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("%s: Message on %s: %s\n", workerName, msg.TopicPartition, string(msg.Value))
+		} else {
+			fmt.Printf("%s: Consumer error: %v (%v)\n", workerName, err, msg)
+			break
+		}
+	}
+	c.Close()
+}
+
 func main() {
-	go func() {
-		c, err := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers": "localhost",
-			"group.id":          "myGroup",
-			"auto.offset.reset": "earliest",
-		})
 
-		if err != nil {
-			panic(err)
-		}
-
-		c.SubscribeTopics([]string{"playwithkafka"}, nil)
-
-		for {
-			msg, err := c.ReadMessage(-1)
-			if err == nil {
-				fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-			} else {
-				fmt.Printf("Consumer error: %v (%v)\n", err, msg)
-				break
-			}
-		}
-		c.Close()
-	}()
+	go consume("GroupC1", "Worker1", "playwithkafka")
+	go consume("GroupC1", "Worker2", "playwithkafka")
+	go consume("GroupC2", "Worker1", "playwithkafka")
+	go consume("GroupC3", "Worker1", "playwithkafka2")
 
 	engine := echo.New()
 	engine.GET("/ping", func(c echo.Context) error {
+		fmt.Println("Recive ping from", c.Request().Host, c.Request().Header.Get("Host"))
 		c.String(http.StatusOK, "pong")
 		return nil
 	})
--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-export HOST_IP_ADDRESS=$(ipconfig getifaddr $(route -n get default|awk '/interface/ { print $2 }'))
 docker-compose up -d
